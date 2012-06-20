d65746751b8daeb17dee0e9826ef928a963e7c5f
Mirko Raca <racamirko@gmail.com>
2012-06-20 10:27:30 +0200

    Size difference penalty. Color space penalty not working.

--- facetrack/CSIRFilterPt.cpp
+++ facetrack/CSIRFilterPt.cpp
@@ -16,7 +16,7 @@ using namespace cv;
 CSIRFilterPt::CSIRFilterPt(cv::Point2i pInitPt, cv::Mat &pInitImg, cv::Point2i pAreaSize)
   : mArea(pAreaSize)
   , mCurrentPosition(pInitPt)
-  , mCurrentSimilarity(0)
+  , mCurrentSimilarity(0.9)
 {
   int shiftX = pAreaSize.x/2,
       shiftY = pAreaSize.y/2;
@@ -30,7 +30,7 @@ CSIRFilterPt::CSIRFilterPt(cv::Point2i pInitPt, cv::Mat &pInitImg, cv::Point2i p
   initRng();
   mDistX = 50.0f;
   mDistY = 50.0f;
-  mNumOfParticles = 10;
+  mNumOfParticles = 20;
 
   unsigned int initPts[] = {0};
   mVecPts.push_back(pInitPt);
@@ -45,22 +45,22 @@ cv::Rect CSIRFilterPt::pt2rect(cv::Point2i pPt, cv::Point2i pAreaSize){
 }
 
 void CSIRFilterPt::getSegment(cv::Mat& pImgData, cv::Rect pRegion, cv::Mat& pSegment){
-  #ifdef _DEBUG
-  Rect origRect = pRegion;
-  #endif
+//  #ifdef _DEBUG
+//  Rect origRect = pRegion;
+//  #endif
 
   pRegion.x = min(max(pRegion.x,0), pImgData.cols-1);
   pRegion.y = min(max(pRegion.y,0), pImgData.rows-1);
   pRegion.width = max(min(pRegion.width, pImgData.cols - pRegion.x),1);
   pRegion.height = max(min(pRegion.height, pImgData.rows - pRegion.y),1);
-  #ifdef _DEBUG
-  if( origRect.x != pRegion.x || origRect.y != pRegion.y ||
-      origRect.width != pRegion.width || origRect.height != pRegion.height )
-  {
-    cout << "Original region " << origRect.x << ", " << origRect.y << ", " << origRect.width << ", " << origRect.height << endl;
-    cout << "Region size changed: " << pRegion.x << ", " << pRegion.y << ", " << pRegion.width << ", " << pRegion.height << endl;
-  }
-  #endif
+//  #ifdef _DEBUG
+//  if( origRect.x != pRegion.x || origRect.y != pRegion.y ||
+//      origRect.width != pRegion.width || origRect.height != pRegion.height )
+//  {
+//    cout << "Original region " << origRect.x << ", " << origRect.y << ", " << origRect.width << ", " << origRect.height << endl;
+//    cout << "Region size changed: " << pRegion.x << ", " << pRegion.y << ", " << pRegion.width << ", " << pRegion.height << endl;
+//  }
+//  #endif
 
   pSegment = pImgData(pRegion);
 }
@@ -80,23 +80,23 @@ void CSIRFilterPt::initRng(){
 void CSIRFilterPt::generatePoints( unsigned int *pProbs, unsigned int pNumOfPoints, unsigned int pNumOfPtsToGenerate ){
   vector<Point2i> vecNewPts;
 
-#ifdef _DEBUG
-  cout << "Original point array" << endl;
-  for( vector<Point2i>::iterator iter = mVecPts.begin(); iter != mVecPts.end(); ++iter ){
-    cout << "[" << iter->x << ", " << iter->y << "] ";
-  }
-  cout << endl;
-#endif
+//#ifdef _DEBUG
+//  cout << "Original point array" << endl;
+//  for( vector<Point2i>::iterator iter = mVecPts.begin(); iter != mVecPts.end(); ++iter ){
+//    cout << "[" << iter->x << ", " << iter->y << "] ";
+//  }
+//  cout << endl;
+//#endif
 
   int ptIdx = 0;
   for(int i = 0; i < pNumOfPtsToGenerate; ++i){
     ptIdx = (int) (pNumOfPoints-1)*gsl_rng_uniform(mRngR);
-    float stepX = gsl_ran_gaussian(mRngR, 0.2)*mDistX,
-          stepY = gsl_ran_gaussian(mRngR, 0.2)*mDistY;
+    float stepX = gsl_ran_gaussian(mRngR, 1-mCurrentSimilarity)*mDistX,
+          stepY = gsl_ran_gaussian(mRngR, 1-mCurrentSimilarity)*mDistY;
     unsigned int ptNum = pProbs[ptIdx];
-#ifdef _DEBUG
-    cout << "using point: " << ptNum << endl;
-#endif
+//#ifdef _DEBUG
+//    cout << "using point: " << ptNum << endl;
+//#endif
     Point2i startPt = mVecPts[ptNum];
     int newX = startPt.x+stepX,
         newY = startPt.y+stepY;
@@ -105,12 +105,12 @@ void CSIRFilterPt::generatePoints( unsigned int *pProbs, unsigned int pNumOfPoin
   // replace the points
   mVecPts.clear();
   copy( vecNewPts.begin(), vecNewPts.end(), back_inserter(mVecPts) );
-#ifdef _DEBUG
-  for( vector<Point2i>::iterator iter = mVecPts.begin(); iter != mVecPts.end(); ++iter ){
-    cout << "[" << iter->x << ", " << iter->y << "] ";
-  }
-  cout << "Check point array" << endl;
-#endif
+//#ifdef _DEBUG
+//  for( vector<Point2i>::iterator iter = mVecPts.begin(); iter != mVecPts.end(); ++iter ){
+//    cout << "[" << iter->x << ", " << iter->y << "] ";
+//  }
+//  cout << "Check point array" << endl;
+//#endif
  }
 
 void CSIRFilterPt::predictNextPos(cv::Mat &pImgData){
@@ -118,6 +118,8 @@ void CSIRFilterPt::predictNextPos(cv::Mat &pImgData){
   float* vecScores = new float[mVecPts.size()];
   const unsigned int PROB_SIZE = 10000;
   unsigned int* vecProbabs = new unsigned int[PROB_SIZE];
+  // init
+  memset(vecProbabs, 0, sizeof(unsigned int)*PROB_SIZE);
   vector<Point2i>::iterator iter;
   unsigned int idx = 0,
                bestIdx = -1;
@@ -127,7 +129,7 @@ void CSIRFilterPt::predictNextPos(cv::Mat &pImgData){
   for( iter = mVecPts.begin(); iter != mVecPts.end(); ++iter ){
     vecScores[idx] = calcScore(pImgData, *iter);
     #ifdef _DEBUG
-      cout << "Point #" << idx << "score: " << vecScores[idx] << endl;
+      cout << "Point #" << idx << " score: " << vecScores[idx] << endl;
     #endif
     totalScore += vecScores[idx];
     if( vecScores[idx] > bestScore ){
@@ -136,6 +138,10 @@ void CSIRFilterPt::predictNextPos(cv::Mat &pImgData){
     }
     ++idx;
   }
+#ifdef _DEBUG
+  cout << "Best score: " << bestScore << endl;
+#endif
+
   // set the best point
   mCurrentPosition = mVecPts[bestIdx];
   mCurrentSimilarity = bestScore;
@@ -165,19 +171,23 @@ float CSIRFilterPt::calcScore(Mat& pImgData, Point2i pt){
   getSegment(pImgData, pt2rect(pt, mArea), segment);
   // todo: if the segment is not the same size as the original image
 
+  float penalty = 0; // penalty for difference in size
+  float maxPenaltyPerPixelChannel = 20.0f; //TODO: heuristics, 20 per channel is a heuristic
   if( segment.rows < mRefData.rows || segment.cols < mRefData.cols ){
     usingResizeData = true;
     unsigned int offsetY = (mRefData.rows - segment.rows)/2,
                  offsetX = (mRefData.cols - segment.cols)/2;
     resizeRefData = mRefData(Rect(offsetX, offsetY, segment.cols, segment.rows));
+    penalty = ( mRefData.rows*mRefData.cols - segment.rows*segment.cols )*maxPenaltyPerPixelChannel;
   }
-#ifdef _DEBUG
-  if(!usingResizeData)
-    imshow("origImage", mRefData);
-  else
-    imshow("origImage",resizeRefData);
-  imshow("PointSampleImage",segment);
-#endif
+//#ifdef _DEBUG
+//  if(!usingResizeData)
+//    imshow("origImage", mRefData);
+//  else
+//    imshow("origImage",resizeRefData);
+//  imshow("PointSampleImage",segment);
+//  waitKey();
+//#endif
 
   Mat diff;
   if(!usingResizeData)
@@ -186,10 +196,10 @@ float CSIRFilterPt::calcScore(Mat& pImgData, Point2i pt){
     cv::absdiff(segment, resizeRefData, diff);
 
   Mat::MSize matSize = mRefData.size;
-  float maxDiff = matSize[0]*matSize[1]*3*20; // 20 per channel is a heuristic
+  float maxDiff = matSize[0]*matSize[1]*3*maxPenaltyPerPixelChannel;
   Scalar scalDiff = sum(diff);
   float curDiff = scalDiff[0]+scalDiff[1]+scalDiff[2];
-  return max(1.0f - curDiff/maxDiff, 0.0f);
+  return max(1.0f - (curDiff+penalty)/maxDiff, 0.0f);
 }
 
 Point2i CSIRFilterPt::getPosition(){
