531b774d9d75bde9ca8cc28e1941f5cd53b7d759
Mirko Raca <mirko.raca@epfl.ch>
2013-02-27 10:36:44 +0100

    test of removing files

--- facetrack/initData.h
+++
@@ -1,9 +0,0 @@
-#ifndef INITDATA_H
-#define INITDATA_H
-
-#include <vector>
-#include <opencv2/core/core.hpp>
-
-void initStartCoord( std::vector<cv::Point2i>& pPersonPoints );
-
-#endif // INITDATA_H
