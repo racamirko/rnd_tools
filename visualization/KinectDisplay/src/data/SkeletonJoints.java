cd1c48534554da96c8b67e3ad66ccc5bfcfcd51b
racamirko <mirko.raca@epfl.ch>
2013-08-23 09:27:31 +0200

    Format reader in progress

--- visualization/KinectDisplay/src/data/SkeletonJoints.java
+++ visualization/KinectDisplay/src/data/SkeletonJoints.java
@@ -1,5 +1,6 @@
 package data;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import processing.core.PVector;
@@ -23,6 +24,45 @@ public class SkeletonJoints {
 			}
 	};
 	
-	Map<enumSkelPart, PVector>	
+	public enum enumPartStatus {
+		SKELPART_STAT_NOT_TRACKED(0), SKELPART_STAT_INFERRED(1), SKELPART_STAT_TRACKED(2);
+		
+		private final int index;
+		
+		enumPartStatus(int index){
+			this.index = index;
+		}
+		
+		int getIndex(){
+			return index;
+		}
+	}
+	
+	public Map<enumSkelPart, PVector> bodyParts;
+	public Map<enumSkelPart, enumPartStatus> bodyPartStatus;
+	static public Map<enumSkelPart, enumSkelPart> mapParentPart = new HashMap<SkeletonJoints.enumSkelPart, SkeletonJoints.enumSkelPart>();
 
+	public static void initSkelPartsParents(){
+		mapParentPart.put(enumSkelPart.SKEL_PELVIS, null);
+		mapParentPart.put(enumSkelPart.SKEL_BACKBONE, enumSkelPart.SKEL_PELVIS);
+		mapParentPart.put(enumSkelPart.SKEL_SHOULDERS_CENTER, enumSkelPart.SKEL_BACKBONE);
+		mapParentPart.put(enumSkelPart.SKEL_SHOULDER_L, enumSkelPart.SKEL_SHOULDERS_CENTER);
+		mapParentPart.put(enumSkelPart.SKEL_ELBOW_L, enumSkelPart.SKEL_SHOULDER_L);
+		mapParentPart.put(enumSkelPart.SKEL_HAND_L, enumSkelPart.SKEL_ELBOW_L);
+		mapParentPart.put(enumSkelPart.SKEL_FINGERS_L, enumSkelPart.SKEL_HAND_L);
+		mapParentPart.put(enumSkelPart.SKEL_SHOULDER_R, enumSkelPart.SKEL_SHOULDERS_CENTER);
+		mapParentPart.put(enumSkelPart.SKEL_ELBOW_R, enumSkelPart.SKEL_SHOULDER_R);
+		mapParentPart.put(enumSkelPart.SKEL_HAND_R, enumSkelPart.SKEL_ELBOW_R);
+		mapParentPart.put(enumSkelPart.SKEL_FINGERS_R, enumSkelPart.SKEL_HAND_R);
+		mapParentPart.put(enumSkelPart.SKEL_PELVIS_L, enumSkelPart.SKEL_PELVIS);
+		mapParentPart.put(enumSkelPart.SKEL_KNEE_L, enumSkelPart.SKEL_PELVIS_L);
+		mapParentPart.put(enumSkelPart.SKEL_ANKLE_L, enumSkelPart.SKEL_KNEE_L);
+		mapParentPart.put(enumSkelPart.SKEL_TOES_L, enumSkelPart.SKEL_ANKLE_L);
+		mapParentPart.put(enumSkelPart.SKEL_PELVIS_R, enumSkelPart.SKEL_PELVIS);
+		mapParentPart.put(enumSkelPart.SKEL_KNEE_R, enumSkelPart.SKEL_PELVIS_R);
+		mapParentPart.put(enumSkelPart.SKEL_ANKLE_R, enumSkelPart.SKEL_KNEE_R);
+		mapParentPart.put(enumSkelPart.SKEL_TOES_R, enumSkelPart.SKEL_ANKLE_R);
+	}
+	
+	
 }
